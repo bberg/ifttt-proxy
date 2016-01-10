01c1fe4ad0b030501dbde0354e08ae023a40b46a
benashbe <benberg00@gmail.com>
2016-01-10 11:26:38 -0500

    test

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,6 @@ var config = require(path.join(__dirname, '../', '../', 'config'));
 var log = require(path.join(__dirname, '../', '../', 'log'));
 var http = require('https')
 
-
 router.post('/api/v1/wmata/trains', function(req, res, next) {
     log.info({"inboundRequest":{"ip":req.ip,"url":"/api/v1/wmata/trains"}})
     log.debug({"inboundRequest":{"ip":req.ip,"url":"/api/v1/wmata/trains","body": req.body,"headers":req.headers}})
