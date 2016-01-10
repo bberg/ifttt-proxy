8c032f3628ecc6b72b1071c1c14e0fce5ed287d8
benashbe <benberg00@gmail.com>
2016-01-10 00:34:07 -0500

    more port config work

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
-console.log(process.env)
+log.error(process.env)
+if(process.env['HOME']=='/home/benashbe'){
+    var port = 19537
+    log.error('RUNNING ON SERVER... PORT: '+port.toString())
+}
+if(process.env['HOME']=='/Users/benberg'){
+    var port = 3000
+    log.error('RUNNING ON DEV... PORT: '+port.toString())
+}
 
-var port = 19537
-// var port = 3000
 var wamata_api_key = '4e7c50268806494dbb109d8ee4495169'
 
 module.exports = wamata_api_key
